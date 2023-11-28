6ab44f19015e7b6bf1e1383038a39b2ecb5925a6
sahidhaqqi <sahidhaqqi123@gmail.com>
2023-11-28 10:15:38 +0700

    Update wordpress.sh
    percobaan2

--- wordpress.sh
+++ wordpress.sh
@@ -1,51 +1,47 @@
 #!/bin/bash
 
-# Update dan upgrade paket
+# Update and upgrade packages
 apt update && apt upgrade
 
-# Instal Apache2
+# Install Apache2
 apt-get install apache2
 
-# Instal MariaDB
-apt install mariadb-server && mariadb-client
+# Install MariaDB
+apt install mariadb-server mariadb-client
 
-# Jalankan skrip keamanan MariaDB & phpmyadmin (manual)
-apt-get install phpmyadmin
-sudo mysql_secure_installation
+# Install PHP
+apt-get install php libapache2-mod-php php-mysql
 
-# Instal PHP
-apt-get install php && libapache2-mod-php && php-mysql
-
-
-# Buat database WordPress
-sudo mysql -u root -p << EOF
+# Create WordPress database
+mysql -u root -p << EOF
 CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
 EOF
 
-# Unduh dan ekstrak WordPress (manual)
-apt-get install unzip
-cd /var/www/html
-rm index.html
-curl --remote-name --silent --show-error https://wordpress.org/latest.tar.gz
-tar --extract --gzip --file latest.tar.gz
-sudo mv wordpress/* .
-rm latest.tar.gz
-rmdir wordpress && rm latest.zip
+# Download and extract WordPress
+wget https://wordpress.org/latest.tar.gz
+tar -xvf latest.tar.gz
 
-# Ubah kepemilikan folder WordPress
-chown -R www-data:www-data /var/www/html/
+# Move WordPress files to the correct location
+sudo mv wordpress/* /var/www/html/
+
+# Delete temporary files
+rm -rf wordpress latest.tar.gz
+rm /var/www/html/index.html
+
+# Change ownership of WordPress files
+sudo chown -R www-data:www-data /var/www/html/
+
+# Secure MariaDB and install phpmyadmin
+sudo mysql_secure_installation
+apt-get install phpmyadmin
+
+# Enable Apache2 modules
+sudo a2enmod rewrite
 
 # Restart Apache2
-systemctl restart apache2
+sudo systemctl restart apache2
 
-# Informasi
-echo "WordPress berhasil diinstal!"
-
-
-
-
-
-
-
+# Information
+echo "WordPress successfully installed!"
