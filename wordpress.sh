61632d9cb2c871260ae48c4fb03ade8683a3bb25
sahidhaqqi <sahidhaqqi123@gmail.com>
2023-11-28 08:56:23 +0700

    Update wordpress.sh
    percobaan1

--- wordpress.sh
+++ wordpress.sh
@@ -28,9 +28,10 @@ EOF
 apt-get install unzip
 cd /var/www/html
 rm index.html
-sudo wget "https://wordpress.org/latest.zip"
-unzip latest.zip
+curl --remote-name --silent --show-error https://wordpress.org/latest.tar.gz
+tar --extract --gzip --file latest.tar.gz
 sudo mv wordpress/* .
+rm latest.tar.gz
 rmdir wordpress && rm latest.zip
 
 # Ubah kepemilikan folder WordPress
@@ -48,31 +49,3 @@ echo "WordPress berhasil diinstal!"
 
 
 
-
-"
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Selamat Datang</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1 class="text-center">Selamat Datang</h1>
-        <p class="text-center">Oleh Sahid Haqqi An Najil</p>
-      </div>
-    </div>
-  </div>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html> 
-"
